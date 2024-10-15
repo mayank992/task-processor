f98882cd0f4dc75222e181ea7ef8bf9d5223818c
Mayank Jindal <mayank.jindal@sprinklr.com>
2024-10-15 21:34:00 +0530

    Adds Solution

--- src/taskProcessor/TaskProcessor.js
+++ src/taskProcessor/TaskProcessor.js
@@ -16,7 +16,69 @@ export class TaskProcessor {
     this.maxRunningTasks = maxRunningTasks;
     this.onUpdateTaskStatus = onUpdateTaskStatus;
 
+    this.completedTasks = new Set();
+
+    this.pendingTasks = []; // list of pending tasks
+    this.taskQueueHigh = []; // high priority queue
+    this.taskQueueNormal = []; // normal priority queue
+
     this.runningTasks = 0;
+  }
+
+  canRun(task) {
+    // check dependencies
+    return task.dependencies.every((depId) => this.completedTasks.has(depId));
+  }
+
+  getNextTask() {
+    // first check high priority tasks
+    if (this.taskQueueHigh.length > 0) {
+      return this.taskQueueHigh.shift();
+    }
+
+    // then check normal priority tasks
+    if (this.taskQueueNormal.length > 0) {
+      return this.taskQueueNormal.shift();
+    }
+
+    // no tasks ready to run
+    return null;
+  }
+
+  processTasks() {
+    while (this.runningTasks < this.maxRunningTasks) {
+      const task = this.getNextTask();
+      if (!task) break; // no tasks ready to run
+
+      this.executeTask(task);
+    }
+  }
+
+  enqueueTask(task) {
+    if (task.type === TASK_TYPES.HIGH_PRIORITY) {
+      this.taskQueueHigh.push(task);
+    } else {
+      this.taskQueueNormal.push(task);
+    }
+
+    // trigger processing when a new task is enqueued
+    this.processTasks();
+  }
+
+  enqueueRunnableTasks() {
+    if (this.pendingTasks.length === 0) {
+      this.processTasks();
+      return;
+    }
+
+    // enqueue tasks that might now be ready
+    this.pendingTasks = this.pendingTasks.reduce((acc, task) => {
+      if (this.canRun(task)) {
+        this.enqueueTask(task);
+        return acc;
+      }
+      return [...acc, task];
+    }, []);
   }
 
   async executeTask(task) {
@@ -28,13 +90,18 @@ export class TaskProcessor {
     this.runningTasks--;
     this.completedTasks.add(task.id);
     this.onUpdateTaskStatus(task.id, TASK_STATUS.COMPLETED);
+
+    // enqueue runnable tasks and process
+    this.enqueueRunnableTasks();
   }
 
   addTask(task) {
-    // complete this function
+    this.pendingTasks.push(task);
+    this.enqueueRunnableTasks();
   }
 
   updateMaxRunningTasks(maxRunningTasks) {
-    // complete this function
+    this.maxRunningTasks = maxRunningTasks;
+    this.enqueueRunnableTasks();
   }
 }
